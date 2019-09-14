081ee73cb0a58939bd5ba9c3bbc80aede24dbba0
Parker Lawrence <liverspleenkidney@gmail.com>
2019-09-14 19:41:11 -0400

    github integration code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template, request, jsonify, redirect
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
 import requests
 # from flask.ext.heroku import Heroku
@@ -14,66 +14,13 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Book
+
+CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
+CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
 
 
 
-
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
-
-# @app.route("/add")
-# def add_book():
-#     name=request.args.get('name')
-#     author=request.args.get('author')
-#     published=request.args.get('published')
-#     try:
-#         book=Book(
-#             name=name,
-#             author=author,
-#             published=published
-#         )
-#         db.session.add(book)
-#         db.session.commit()
-#         return "Book added. book id={}".format(book.id)
-#     except Exception as e:
-# 	    return(str(e))
-
-# @app.route("/getall")
-# def get_all():
-#     try:
-#         books=Book.query.all()
-#         return  jsonify([e.serialize() for e in books])
-#     except Exception as e:
-# 	    return(str(e))
-
-# @app.route("/get/<id_>")
-# def get_by_id(id_):
-#     try:
-#         book=Book.query.filter_by(id=id_).first()
-#         return jsonify(book.serialize())
-#     except Exception as e:
-# 	    return(str(e))
-
-# @app.route("/add/form",methods=['GET', 'POST'])
-# def add_book_form():
-#     if request.method == 'POST':
-#         name=request.form.get('name')
-#         author=request.form.get('author')
-#         published=request.form.get('published')
-#         try:
-#             book=Book(
-#                 name=name,
-#                 author=author,
-#                 published=published
-#             )
-#             db.session.add(book)
-#             db.session.commit()
-#             return "Book added. book id={}".format(book.id)
-#         except Exception as e:
-#             return(str(e))
-#     return render_template("getdata.html")
+from models import Session, TemFile
 
 
 
@@ -82,10 +29,14 @@ from models import Book
 def projectlist():
 	return render_template('todolist.html')
 
+@app.route("/editor")
+def editor():
+	return render_template('editor.html')
 
 @app.route("/login")
 def login():
-	return "soidfjso"
+	return redirect("https://github.com/login/oauth/authorize?scope=repo&client_id="+CLIENT_ID, code=302)
+
 
 
 @app.route("/logout")
@@ -93,9 +44,136 @@ def logout():
 	return "jw9ejfoweufj"
 
 
-@app.route("/editor")
-def editor():
-	return render_template('editor.html')
+@app.route("/gitcallback")
+def gitcallback():
+	session_code = request.args.get('code')
+	res = requests.post('https://github.com/login/oauth/access_token',params={'accept':'json'},data={'client_id':CLIENT_ID,'client_secret':CLIENT_SECRET,'code':session_code})
+	res = res.json()
+	print(res)
+	access_token = res['access_token']
+	user = User(
+		path = request.json['path'],
+		content = con['content'],
+		sha = con['sha']
+	)
+	db.session.add(user)
+	db.session.commit()
+
+
+
+	# session = User.query.filter_by(id=request.json['sessionId']).first()
+
+
+	# extract the token and granted scopes
+
+
+
+	# requests.get('https://api.github.com/events',params=payload)
+	# requests.post('https://httpbin.org/post',params=payload,data={'key':'value'})
+
+
+
+
+
+
+
+
+
+
+@app.route("/files")
+def files():
+	session = Session.query.filter_by(id=request.json['sessionId']).first()
+	if session == None: return
+	book = TemFile.query.filter_by(session_id = session.id,path=request.json['path']).first()
+	if book == None:
+		r = requests.get("https://api.github.com/repos/"+session.owner+"/"+session.repo+"/contents/"+request.json['path']+"?ref="+session.branch)
+		if r.status_code != 200:
+			print(r.content)
+			return
+		con = r.json()
+		book = TemFile(
+			session_id = session.id,
+			path = request.json['path'],
+			content = con['content'],
+			sha = con['sha']
+		)
+		db.session.add(book)
+		db.session.commit()
+	return book.content
+
+
+
+
+@socketio.on('edit')
+def handle_edit(edit):
+	session = Session.query.filter_by(id=request.json['sessionId']).first()
+	if session == None: return
+	book = TemFile.query.filter_by(session_id = session.id,path=request.json['path']).first()
+	if book == None: return
+	book.content = book.content[:edit.delta.amt]+edit.delta.msg+book.content[edit.delta.amt:]
+	db.session.commit()
+	emit('edit',edit,include_self=False)
+
+
+	
+
+@app.route("/directories")
+def directories():
+	session = Session.query.filter_by(id=request.json['sessionId']).first()
+	if session == None: return
+	r = requests.get("https://api.github.com/repos/"+session.owner+"/"+session.repo+"/git/trees/"+session.sha+"?recursive=1,ref="+session.branch)
+	if r.status_code != 200:
+		print(r.content)
+		return
+	return r.json()
+
+
+@socketio.on('join')
+def on_join(data):
+	repo = Session.query.filter_by(owner=data['owner'],repo=data['repo']).first()
+	if repo == None: return
+
+	sesh = Session.query.filter_by(project_id=repo.id).first()
+	if sesh == None:
+		master = requests.get("https://api.github.com/repos/"+session.owner+"/"+session.repo+"/branches/master",headers={'Authorization':'token '+github_token})
+		head_tree_sha = master.json()['commit']['commit']['tree']['sha']
+		sesh = Session(
+			owner      = repo.owner,
+			repo       = repo.repo,
+			branch     = repo.branch,
+			sha        = head_tree_sha,
+			project_id = repo.id,
+			activemembers = str(request.sid)
+		)
+		db.session.add(sesh)
+	else:
+		sesh.activemembers = sesh.activemembers+","+str(request.sid)
+	db.session.commit()
+
+	join_room(str(repo.id)+","+str(sesh.id))
+	emit('accept',{'sessionId':sesh.id,'activemembers':sesh.activemembers},room=request.sid)
+	emit('player_join',{'name':request.sid},room=str(repo.id)+","+str(sesh.id),include_self=False)
+
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+	for sesh in Session.query:
+		jj = sesh.activemembers.split(",")
+		if request.sid in jj:
+			jj.remove(request.sid)
+		sesh.activemembers = ",".join(jj)
+	db.session.commit()
+	emit('player_leave',{'name':request.sid},include_self=False)
+
+
+
+
+
+
+
+
+
 
 
 # @app.route("/projects",methods=['GET','POST'])
@@ -109,21 +187,6 @@ def editor():
 
 # @socketio.on('edit')
 # def handle_my_custom_event(json):
-
-
-@socketio.on('connect')
-def handle_my_custom_connect():
-	print("syjyyok")
-
-
-@socketio.on('disconnect')
-def on_disconnect():
-	print('Client disconnected')
-
-
-@socketio.on('edit')
-def handle_edit(edit):
-    emit('edit',edit,broadcast=True)
 
 
 
--- models.py
+++ models.py
@@ -1,17 +1,22 @@
 from app import db
+import datetime
 
-class Book(db.Model):
-    __tablename__ = 'books'
-
+class Project(db.Model):
+    __tablename__ = 'projects'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    author = db.Column(db.String())
-    published = db.Column(db.String())
+    description = db.Column(db.String())
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    owner = db.Column(db.String())
+    repo = db.Column(db.String())
+    branch = db.Column(db.String())
 
-    def __init__(self, name, author, published):
+    def __init__(self, name,description,owner,repo,branch):
         self.name = name
-        self.author = author
-        self.published = published
+        self.description = description
+        self.owner = owner
+        self.repo = repo
+        self.branch = branch
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -20,8 +25,112 @@ class Book(db.Model):
         return {
             'id': self.id, 
             'name': self.name,
-            'author': self.author,
-            'published':self.published
+            'description': self.description,
+            'created_date': self.created_date,
+            'likes':self.likes,
+            'owner':self.owner,
+            'repo':self.repo,
+            'branch':self.branch,
         }
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String())
+    author = db.Column(db.String())
+    project_id = db.Column(db.Integer, db.ForeignKey('Project.id'))
+
+    def __init__(self, message, author, project):
+        self.message = message
+        self.author = author
+        self.project_id = project
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'message': self.message,
+            'author': self.author
+        }
+
+
+class Session(db.Model):
+    __tablename__ = 'sessions'
+
+    id         = db.Column(db.Integer, primary_key=True)
+    owner      = db.Column(db.String())
+    repo       = db.Column(db.String())
+    branch     = db.Column(db.String())
+    sha        = db.Column(db.String())
+    project_id = db.Column(db.Integer, db.ForeignKey('Project.id'))
+    activemembers = db.Column(db.String())
+
+    def __init__(self, owner, repo, branch, sha,project_id,activemembers):
+        self.owner = owner,
+        self.repo = repo,
+        self.branch = branch,
+        self.sha = sha,
+        self.project_id = project_id,
+        self.activemembers = activemembers
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'message': self.message,
+            'author': self.author
+        }
+
+
+
+
+class TemFile(db.Model):
+    __tablename__ = 'temfiles'
+
+    id         = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer, db.ForeignKey('Session.id'))
+    path       = db.Column(db.String())
+    content    = db.Column(db.String())
+    sha        = db.Column(db.String())
+
+    def __init__(self,session_id,path,content,sha):
+        self.session_id = session_id
+        self.path = path
+        self.content = content
+        self.sha = sha
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id         = db.Column(db.Integer, primary_key=True)
+    token      = db.Column(db.String())
+    username   = db.Column(db.String())
+
+    def __init__(self,token,username):
+        self.token = token
+        self.username = username
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
