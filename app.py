e7abe8ce98e5bca6e081d89cf461a3ad69023399
Parker Lawrence <liverspleenkidney@gmail.com>
2019-09-14 11:55:21 -0400

    added header stuff

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, send, emit
 from flask_sqlalchemy import SQLAlchemy
+import requests
 # from flask.ext.heroku import Heroku
 import os
 
@@ -18,61 +19,120 @@ from models import Book
 
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
 
-@app.route("/add")
-def add_book():
-    name=request.args.get('name')
-    author=request.args.get('author')
-    published=request.args.get('published')
-    try:
-        book=Book(
-            name=name,
-            author=author,
-            published=published
-        )
-        db.session.add(book)
-        db.session.commit()
-        return "Book added. book id={}".format(book.id)
-    except Exception as e:
-	    return(str(e))
+# @app.route("/add")
+# def add_book():
+#     name=request.args.get('name')
+#     author=request.args.get('author')
+#     published=request.args.get('published')
+#     try:
+#         book=Book(
+#             name=name,
+#             author=author,
+#             published=published
+#         )
+#         db.session.add(book)
+#         db.session.commit()
+#         return "Book added. book id={}".format(book.id)
+#     except Exception as e:
+# 	    return(str(e))
 
-@app.route("/getall")
-def get_all():
-    try:
-        books=Book.query.all()
-        return  jsonify([e.serialize() for e in books])
-    except Exception as e:
-	    return(str(e))
+# @app.route("/getall")
+# def get_all():
+#     try:
+#         books=Book.query.all()
+#         return  jsonify([e.serialize() for e in books])
+#     except Exception as e:
+# 	    return(str(e))
 
-@app.route("/get/<id_>")
-def get_by_id(id_):
-    try:
-        book=Book.query.filter_by(id=id_).first()
-        return jsonify(book.serialize())
-    except Exception as e:
-	    return(str(e))
+# @app.route("/get/<id_>")
+# def get_by_id(id_):
+#     try:
+#         book=Book.query.filter_by(id=id_).first()
+#         return jsonify(book.serialize())
+#     except Exception as e:
+# 	    return(str(e))
 
-@app.route("/add/form",methods=['GET', 'POST'])
-def add_book_form():
-    if request.method == 'POST':
-        name=request.form.get('name')
-        author=request.form.get('author')
-        published=request.form.get('published')
-        try:
-            book=Book(
-                name=name,
-                author=author,
-                published=published
-            )
-            db.session.add(book)
-            db.session.commit()
-            return "Book added. book id={}".format(book.id)
-        except Exception as e:
-            return(str(e))
-    return render_template("getdata.html")
+# @app.route("/add/form",methods=['GET', 'POST'])
+# def add_book_form():
+#     if request.method == 'POST':
+#         name=request.form.get('name')
+#         author=request.form.get('author')
+#         published=request.form.get('published')
+#         try:
+#             book=Book(
+#                 name=name,
+#                 author=author,
+#                 published=published
+#             )
+#             db.session.add(book)
+#             db.session.commit()
+#             return "Book added. book id={}".format(book.id)
+#         except Exception as e:
+#             return(str(e))
+#     return render_template("getdata.html")
+
+
+
+
+@app.route("/projectlist")
+def projectlist():
+	return render_template('todolist.html')
+
+
+@app.route("/login")
+def login():
+	return "soidfjso"
+
+
+@app.route("/logout")
+def logout():
+	return "jw9ejfoweufj"
+
+
+@app.route("/editor")
+def editor():
+	return render_template('editor.html')
+
+
+# @app.route("/projects",methods=['GET','POST'])
+# @app.route("/projects/<int:id>",methods=['GET','PUT','DELETE'])
+
+
+# @app.route("/projects/<int:id>/requests",methods=['GET','POST'])
+# @app.route("/projects/<int:id>/requests/<int:rid>",methods=['DELETE'])
+
+
+
+# @socketio.on('edit')
+# def handle_my_custom_event(json):
+
+
+@socketio.on('connect')
+def handle_my_custom_connect():
+	print("syjyyok")
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+	print('Client disconnected')
+
+
+@socketio.on('edit')
+def handle_my_custom_event(json):
+	print(json)
+
+
+
+
+
+
+# def getfiles():
+# 	response = requests.get("http://127.0.0.1:5000")
+
 
 
 
