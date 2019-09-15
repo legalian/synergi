61c160b8fc414972764e0551b1f794e080b2aa9f
Parker Lawrence <liverspleenkidney@gmail.com>
2019-09-15 05:09:38 -0400

    alsdfjaosidjf merge conflict

--- app.py
+++ app.py
@@ -60,6 +60,10 @@ def repos():
 	openrepos = []
 	res = []
 	for i in github.get("/user/repos").json():
+		print("accessing:"+str(i['owner']['login'])+","+str(i['name']))
+		print(Project.query.all())
+
+		
 		repo = Project.query.filter_by(owner=str(i['owner']['login']),repo=str(i['name'])).first()
 		if repo != None:
 			res.append(repo.serialize())
